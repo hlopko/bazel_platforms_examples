5429ed09a968029da420750bb16ec8d21112fd80
Marcel Hlopko <hlopko@google.com>
2019-06-16 12:03:00 +0200

    Polish example 3

--- sanity_check.sh
+++ sanity_check.sh
@@ -11,3 +11,5 @@ function b() {
 b //examples/01_hello_world:a
 b //examples/02_using_different_platforms:a --platforms=//:linux_platform
 b //examples/02_using_different_platforms:a --platforms=//:windows_platform
+b //examples/03_target_not_compatible_with_constraint:a --platforms=//:linux_platform
+b //examples/03_target_not_compatible_with_constraint:b --platforms=//:linux_platform && (exit 15) || true
